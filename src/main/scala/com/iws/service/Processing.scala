5bf68d7759c6b185e4929b633f73b14a429a25da
B.Mady <kaba.bintou-mady_fr@guj.de>
2019-10-22 08:44:15 +0200

    change the visibility of postDebit, postCredit and postAccount function

--- src/main/scala/com/iws/service/Processing.scala
+++ src/main/scala/com/iws/service/Processing.scala
@@ -6,11 +6,11 @@ object  Processing  {
 
   trait ProcessTransaction [A<:IWS] {
     def post (transaction:A):List[PeriodicAccountBalance]
-    def postDebit ( periode:Int, accountId:String, amount:BigDecimal, company:String, currency:String , modelId:Int):PeriodicAccountBalance =
+    private [Processing] def postDebit ( periode:Int, accountId:String, amount:BigDecimal, company:String, currency:String , modelId:Int):PeriodicAccountBalance =
       postAccount(periode, accountId, amount,true, company,currency, modelId )
-    def postCredit ( periode:Int, accountId:String,amount:BigDecimal , company:String, currency:String, modelId:Int )=
+    private [Processing]  def postCredit ( periode:Int, accountId:String,amount:BigDecimal , company:String, currency:String, modelId:Int )=
       postAccount(periode, accountId, amount,false, company,currency,modelId )
-    private [ProcessTransaction] def  postAccount(periode:Int, accountId:String, amount:BigDecimal,isDebit:Boolean, company:String, currency:String, modelId:Int)
+    private [this] def  postAccount(periode:Int, accountId:String, amount:BigDecimal,isDebit:Boolean, company:String, currency:String, modelId:Int)
       = {
       val map:Map[String,PeriodicAccountBalance]=IWSCache.get(modelId).groupBy(_.id).mapValues(_.head).asInstanceOf[Map[String,PeriodicAccountBalance]] //asInstanceOf[Map[String,List[PeriodicAccountBalance]]]
       val pacc=map.getOrElse(periode.toString.concat(accountId),
