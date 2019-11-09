ddbcd9855654ed587ea8d8718e7c57415cbd0e78
B.Mady <kaba.bintou-mady_fr@guj.de>
2019-11-09 11:35:45 +0100

    minor refactoring

--- src/main/scala/com/iws/service/Processing.scala
+++ src/main/scala/com/iws/service/Processing.scala
@@ -20,7 +20,14 @@ import com.iws.model._
       val pacc=getMapFor(modelId).getOrElse(periode.toString.concat(accountId),
         PeriodicAccountBalance(periode.toString.concat(accountId), accountId, periode, BigDecimal(0),BigDecimal(0),
           BigDecimal(0),  BigDecimal(0), company, currency,modelId ))
-      if (isDebit) pacc.copy(debit=pacc.debit+amount) else pacc.copy(credit = pacc.credit + amount)
+      if (isDebit) {
+        updateFollowingBalances(periode,accountId, pacc.debit+amount, modelId, isDebit)
+        pacc.copy(debit=pacc.debit+amount)
+
+      } else {
+        updateFollowingBalances(periode,accountId, pacc.credit+amount, modelId, isDebit)
+        pacc.copy(credit = pacc.credit + amount)
+      }
 
     }
     def updatePacc (pacc:PeriodicAccountBalance, amount:BigDecimal, isDebit:Boolean) =
@@ -29,9 +36,9 @@ import com.iws.model._
     else
         pacc.copy(icredit = pacc.credit+amount)
 
-    def updateFollowingBalances(currentPeriode:Int,nextPeriode:Int,accountId:String, amount:BigDecimal, modelId:Int, isDebit:Boolean): Unit ={
+    def updateFollowingBalances(currentPeriode:Int,accountId:String, amount:BigDecimal, modelId:Int, isDebit:Boolean): Unit ={
      val l =getMapFor(modelId)
-       for( i<- currentPeriode to nextPeriode)
+       for( i<- currentPeriode to 12-currentPeriode)
        l.get(i.toString.concat(accountId)).toList.map(updatePacc(_, amount,isDebit ))
        //val map:Map[String,PeriodicAccountBalance]=IWSCache.get(modelId).groupBy(_.id).mapValues(_.head)
        // .asInstanceOf[Map[String,PeriodicAccountBalance]]
